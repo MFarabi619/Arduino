3f4ca424a84ddc8791654d7edf2f492c9581a704
Mumtahin Farabi <mfarabi619@gmail.com>
2023-04-14 16:56:43 -0400

    chore: Create setup and define variables for colors

--- RGB LED/RGB LED.ino
+++ RGB LED/RGB LED.ino
@@ -3,3 +3,16 @@
 #define GREEN 5
 #define RED 6
 
+void setup(){
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
+}
+
+// define variables
+int redValue;
+int greenValue;
+int blueValue;
