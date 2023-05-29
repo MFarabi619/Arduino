013c606f9becdc48769b16310669bc62cc9402d0
Mumtahin Farabi <mfarabi619@gmail.com>
2023-05-28 23:52:47 -0400

    chore: Create code file and add setup

---
+++ 4 LED Digital Output/4 LED Digital Output.ino
@@ -0,0 +1,12 @@
+int LED0 = 2; // Use pin 2 to control the white LED
+int LED1 = 3; // Use pin 3 to control the yellow LED
+int LED2 = 4; // Use pin 4 to control the green LED
+int LED3 = 5; // Use pin 5 to control the red LED
+
+void setup(){
+    //initialize digital pin 2 to 5 as output:
+    pinMode(LED0, OUTPUT);
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+}
