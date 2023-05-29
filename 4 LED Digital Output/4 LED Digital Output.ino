776f5fd7390d0e7020cf358545b5b6e61b015451
Mumtahin Farabi <mfarabi619@gmail.com>
2023-05-29 01:02:44 -0400

    feat: Time delay for LEDs using Potentiometer

--- 4 LED Digital Output/4 LED Digital Output.ino
+++ 4 LED Digital Output/4 LED Digital Output.ino
@@ -3,6 +3,7 @@ int LED1 = 3;       // Use pin 3 to control the yellow LED
 int LED2 = 4;       // Use pin 4 to control the green LED
 int LED3 = 5;       // Use pin 5 to control the red LED
 int delayTime = 50; // Delay time between each LED change in milliseconds
+int sensorValue = 0; // Variable to store the value coming from the potentiometer
 
 void setup()
 {
@@ -11,6 +12,12 @@ void setup()
     pinMode(LED1, OUTPUT);
     pinMode(LED2, OUTPUT);
     pinMode(LED3, OUTPUT);
+
+    //initialize analog pin as input:
+    pinMode(A0, INPUT);
+
+    // initialize serial communication at 9600 bits per second:
+    Serial.begin(9600); 
 }
 
 void lightUpLEDs(int LED, int delayTime)
@@ -22,6 +29,10 @@ void lightUpLEDs(int LED, int delayTime)
 
 void loop()
 {
+    sensorValue = analogRead(A0); // read the input on analog pin 0
+    delayTime = map(sensorValue, 0, 1023, 30, 255); // Set delay time based on the potentiometer value
+    Serial.println(delayTime); // print out the value of delayTime
+
     lightUpLEDs(LED0, delayTime);
     lightUpLEDs(LED1, delayTime);
     lightUpLEDs(LED2, delayTime);
