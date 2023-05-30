39aa39d3582f899147cdcbca2035d98cb0ef8981
Mumtahin Farabi <mfarabi619@gmail.com>
2023-05-29 22:30:31 -0400

    feat: Buzzer generates tone

--- Active Buzzer/Active Buzzer.ino
+++ Active Buzzer/Active Buzzer.ino
@@ -1,10 +1,16 @@
 int buzzer = 12; // Assign pin 12 for buzzer
+int delayTime = 200; // Set delay in milliseconds
 
 void setup()
 {
- pinMode(buzzer, OUTPUT); // Initialize the buzzer pin as an output
+    pinMode(buzzer, OUTPUT); // Initialize the buzzer pin as an output
 }
 
 void loop()
 {
-} 
+    // Output a tone. Active buzzers can only output a single tone.
+    digitalWrite(buzzer, HIGH);
+    delay(delayTime); 
+    digitalWrite(buzzer, LOW);
+    delay(delayTime); 
+}
