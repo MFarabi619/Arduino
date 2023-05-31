b8ba55c3fc85bd8541b9d69acabe709412731ecd
Mumtahin Farabi <mfarabi619@gmail.com>
2023-05-31 00:59:17 -0400

    feat: Working Passive Buzzer with potentiometer

--- Passive Buzzer/Passive Buzzer.ino
+++ Passive Buzzer/Passive Buzzer.ino
@@ -1,8 +1,20 @@
+#include "pitches.h"
 
-void setup(){
+int potentiometer = A0; // potentiometer connected to analog pin 0
+int buzzer = 12;        // buzzer connected to digital pin 11
+const int G_MIXOLYDIAN_SCALE[] = {NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5};
 
+void setup()
+{
+    pinMode(potentiometer, INPUT); // sets the pin as input
+    pinMode(buzzer, OUTPUT);       // sets the pin as output
+    Serial.begin(9600);            // starts the serial communication
 }
 
-void loop(){
-
+void loop()
+{
+    int pitch = analogRead(potentiometer); // reads the value of the potentiometer (value between 0 and 1023)
+    pitch = map(pitch, 0, 1023, 0, 7);     // scale it to use it with the tone function (value between 100 and 1000)
+    tone(buzzer, G_MIXOLYDIAN_SCALE[pitch % 8]); // plays the note in pitch
+    delay(500);
 }
