fad026e066ee82456259b7500fbbbb6523f60470
Mumtahin Farabi <mfarabi619@gmail.com>
2023-06-03 13:01:00 -0400

    feat: Add code to make servo sweep

--- Servo Motor/Servo Motor.ino
+++ Servo Motor/Servo Motor.ino
@@ -5,23 +5,20 @@ Servo myservo; // create servo object to control a servo
 int potpin = 0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
 
-void setup() {
-  myservo.attach(2);  // attaches the servo on pin 2 to the servo object
+void setup()
+{
+    myservo.attach(2); // attaches the servo on pin 2 to the servo object
 }
 
-void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 1023, 0, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
+void loop()
+{
+    val = analogRead(potpin);        // reads the value of the potentiometer (value between 0 and 1023)
+    val = map(val, 1023, 0, 0, 180); // scale it to use it with the servo (value between 0 and 180)
+    myservo.write(val);              // sets the servo position according to the scaled value
+    delay(15);                       // waits for the servo to get there
 }
 
 // int pos = 0; // variable to store the servo position
-
-// void setup()
-// {
-//     myservo.attach(2); // attaches the servo on pin 9 to the servo object
-// }
 
 // void loop()
 // {
