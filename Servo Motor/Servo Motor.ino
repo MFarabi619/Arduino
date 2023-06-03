f52af929f2e1b424c295fb53738ae945aef4248d
Mumtahin Farabi <mfarabi619@gmail.com>
2023-06-03 12:54:00 -0400

    fix: Servo motor jittering and moving past 180 degrees

--- Servo Motor/Servo Motor.ino
+++ Servo Motor/Servo Motor.ino
@@ -1,18 +1,39 @@
+#include <Servo.h>
 
-int potentiometer = A0; // Set pin for potentiometer input
-int servo = 2;          // Set pin for servo output
-int angle = 0;          // Set initial angle to 0
+Servo myservo; // create servo object to control a servo
 
-void setup()
-{
-    pinMode(potentiometer, INPUT); // Set pin as input
-    Serial.begin(9600);            // Start serial communication
-    pinMode(servo, OUTPUT);        // Set pin as output
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+
+void setup() {
+  myservo.attach(2);  // attaches the servo on pin 2 to the servo object
 }
-void loop()
-{
-    angle = analogRead(potentiometer);   // Read potentiometer value
-    angle = map(angle, 0, 1023, 0, 270); // Scale it to use it with the servo (value between 0 and 180)
-    Serial.println(angle);               // Print potentiometer value
-    analogWrite(servo, angle);           // Write potentiometer value to servo
+
+void loop() {
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  val = map(val, 1023, 0, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  myservo.write(val);                  // sets the servo position according to the scaled value
+  delay(15);                           // waits for the servo to get there
 }
+
+// int pos = 0; // variable to store the servo position
+
+// void setup()
+// {
+//     myservo.attach(2); // attaches the servo on pin 9 to the servo object
+// }
+
+// void loop()
+// {
+//     for (pos = 0; pos <= 180; pos += 10)
+//     { // goes from 0 degrees to 180 degrees
+//         // in steps of 1 degree
+//         myservo.write(pos); // tell servo to go to position in variable 'pos'
+//         delay(15);          // waits 15ms for the servo to reach the position
+//     }
+//     for (pos = 180; pos >= 0; pos -= 10)
+//     {                       // goes from 180 degrees to 0 degrees
+//         myservo.write(pos); // tell servo to go to position in variable 'pos'
+//         delay(15);          // waits 15ms for the servo to reach the position
+//     }
+// }
