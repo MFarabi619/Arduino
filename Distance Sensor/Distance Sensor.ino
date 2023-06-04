06fbf24174e25f8df8ae04745fc5e95cedc4f232
Mumtahin Farabi <mfarabi619@gmail.com>
2023-06-04 15:34:09 -0400

    feat: Push buttons change units for displays independently

--- Distance Sensor/Distance Sensor.ino
+++ Distance Sensor/Distance Sensor.ino
@@ -1,52 +1,93 @@
 #include <LiquidCrystal.h>
+#include <DHT.h>
 
 // LiquidCrystal(RS, E, D4, D5, D6, D7)
 // RS, E, D4, D5, D6, D7 are the LCD pins.
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-int LED_CONTRAST_PIN = 10;  // pin for contrast
-int LED_BACKLIGHT_PIN = 11; // pin for backlight
-int TRIGGER_PIN = 12;    // pin for distance sensor trigger
-int ECHO_PIN = 13;       // pin for distance sensor echo
-int BUTTON_PIN = 53; // pin for button
-// const SPEEDOFSOUND = 0;
-float duration, distanceMm, distanceIn = 0; // Variables for distance sensor
-int buttonState, lastButtonState = HIGH;    // Variables for button state
-bool isOn, isButtonPressed = false;         // Variables to check if button is pressed
+LiquidCrystal LCD1(33, 31, 29, 27, 25, 23);
+LiquidCrystal LCD2(8, 9, 4, 5, 6, 7);
 
+int LCD2_CONTRAST_PIN = 10;  // pin for LCD2 contrast
+int LCD2_BACKLIGHT_PIN = 11; // pin for LCD2 backlight
+
+// Custom character for the degree symbol
+// byte degreeSymbol[8] = {
+//     B00110,
+//     B01001,
+//     B01001,
+//     B00110,
+//     B00000,
+//     B00000,
+//     B00000,
+//     B00000};
+
+int TRIGGER_PIN = 12;                       // pin for distance sensor trigger
+int ECHO_PIN = 13;                          // pin for distance sensor echo
+float duration, distanceMm, distanceIn = 0; // Variables for distance sensor
+
+int BUTTON_PIN[] = {39, 35};             // pins for buttons
+int buttonState[] = {HIGH, HIGH};        // Variable for curent button state
+int lastButtonState[] = {HIGH, HIGH};    // Variable for last button state
+bool isOn[] = {false, false};            // Variable to check if button is on
+bool isButtonPressed[] = {false, false}; // Variables to check if button is pressed
+int reading[] = {HIGH, HIGH};            // Variable to read button state
+
+int DHT_PIN = 3; // pin for DHT sensor
+#define DHTTYPE DHT22
+DHT dht(DHT_PIN, DHTTYPE);
+int temperature, humidity = 0; // Variables for temperature and humidity
+// const SPEEDOFSOUND = 0; // Speed of sound in air
 
 void setup()
 {
-  analogWrite(LED_CONTRAST_PIN, 80);
-  analogWrite(LED_BACKLIGHT_PIN, 255);
+  LCD1.begin(16, 2);
+  analogWrite(LCD2_CONTRAST_PIN, 80);
+  analogWrite(LCD2_BACKLIGHT_PIN, 255);
+  LCD2.begin(16, 2);
+  // // Define the custom character for the degree symbol
+  // LCD1.createChar(0, degreeSymbol);
+  // LCD2.createChar(0, degreeSymbol);
+
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  lcd.begin(16, 2);
+
+  pinMode(BUTTON_PIN[0], INPUT_PULLUP);
+  pinMode(BUTTON_PIN[1], INPUT_PULLUP);
+  pinMode(BUTTON_PIN[2], INPUT_PULLUP);
+
+  dht.begin();
+
   Serial.begin(9600);
 }
 
-bool detectButton(){
+bool detectButton(int num)
+{
   // Read the current state of the button
-  int reading = digitalRead(BUTTON_PIN);
+  reading[num] = digitalRead(BUTTON_PIN[num]);
 
   // Check if the button state has changed
-  if (reading != lastButtonState) {
-    if (reading == LOW) {
-      isButtonPressed = true;  // Set the button press flag
+  if (reading[num] != lastButtonState[num])
+  {
+    Serial.println("Button pressed. Button pin: " + String(BUTTON_PIN[num]));
+    if (reading[num] == LOW)
+    {
+      isButtonPressed[num] = true; // Set the button press flag
     }
   }
 
   // Check if the button has been released
-  if (isButtonPressed && reading == HIGH) {
-    isButtonPressed = false;  // Reset the button press flag
-    
+  if (isButtonPressed[num] && reading[num] == HIGH)
+  {
+    isButtonPressed[num] = false; // Reset the button press flag
+
     // Toggle the on/off state
-    isOn = !isOn;
+    isOn[num] = !isOn[num];
   }
 
   // Update the last button state for comparison in the next iteration
-  lastButtonState = reading;
-  return isOn;
+  lastButtonState[num] = reading[num];
+
+  Serial.println("Button state: " + String(isOn[num]));
+  return isOn[num];
 }
 
 void loop()
@@ -67,20 +108,27 @@ void loop()
   // Determine with button if distance is in mm or in
   String distanceMmString = "Dist(mm): " + String(distanceMm);
   String distanceInString = "Dist(in): " + String(distanceIn);
-  String displayString = detectButton() ? distanceInString : distanceMmString;
+  String displayDistanceString = detectButton(0) ? distanceInString : distanceMmString;
 
+  // Read temperature as Celsius (the default)
+  float tempCelcius = dht.readTemperature();
+  float tempFahrenheit = dht.readTemperature() * 1.8 + 32.0;
 
   // Calculate temperature in degrees Celsius
-  
+  // char tempCelciusString[20];
+  // sprintf(tempCelciusString, "Temp(%cC): %d", tempCelcius, byte(0));
+  String tempCelciusString = "Temp(C): " + String(tempCelcius);
+  String tempFahrenheitString = "Temp(F): " + String(tempFahrenheit);
+  String displayTempString = !detectButton(1) ? tempCelciusString : tempFahrenheitString;
 
   // Print the measured distance, temperature, and humidity
-  Serial.print(displayString + "\n");
-  // Serial.print(distanceInString + "\n");
+  // Serial.print(displayDistanceString + "\n");
 
-  lcd.setCursor(0, 0);
-  lcd.print(displayString);
-  // lcd.setCursor(0, 1);
-  // lcd.print(distanceInString);
+  LCD1.setCursor(0, 0);
+  LCD1.print(displayDistanceString);
+  LCD2.setCursor(0, 0);
+  LCD2.print(displayTempString);
   delay(300);
-  lcd.clear();
+  LCD1.clear();
+  LCD2.clear();
 }
--- Distance Sensor/README.md
+++ Distance Sensor/README.md
@@ -1,4 +1,4 @@
-This project demonstrates the use of and Ultrasonic Sensor to detect distance.
+This project demonstrates the use of an Ultrasonic Sensor and Temperature + Humidity Sensor to measure distance.
 
 ## Live Demo
 [comment]: # (insert video in the next line)
@@ -27,10 +27,17 @@ This project demonstrates the use of and Ultrasonic Sensor to detect distance.
 
 ## Features:
 - The distance detected by the ultrasonic sensor is shown on the LCD 1602 display.
-- While most tutorials use libraries and show the distance measured in centimeters, and does not take into account that the speed of sound is affected by the density of the air. The density of air is determined by temperature, pressure, and humidity. This project uses a temperature sensor to take those factors into account, and provide a much more accurate measurement, in millimeters.
+- While most tutorials use libraries and show the distance measured in centimeters, and does not take into account that the speed of sound is affected by the density of the air. The density of air is determined by temperature, pressure, and humidity. This project uses a temperature sensor to take those factors into account, and provide a much more accurate measurement, in millimeters to two decimal places.
+- The units of display can be changed between metric and imperial by the two buttons, with the top button changing distance and the buttom button changing temperature.
+- A single potentiometer control the contrast on both LCD displays.
 
 ## Learning Log:
-- If the delay within the loop is too short, the value on the LCD constantly updates too quickly making it difficult to read. However a delay too high results in too long of wait for the value to update.
+- If the delay within the loop is too short, the value on the LCD updates too quickly making it difficult to read. However a delay too high results in too long of wait for the value to update.
 - Using solid core wires to make neater circuits instead of using dupont wires.
-- Toggling between on/off states using a single push button, code can be found in the detectButton() function.
-- 
+- The degree symbol cannot be displayed on the LCD 1602 display, and I'm still trying to find out why exactly. Instead, the letter 'C' is used to represent degrees Celsius.
+  - I've tried making a custom symbol using the byte array and storing it in the LCD memory, but it displays '3' instead of the degree symbol.
+- Instead of running the V0 pins of the LCDs into separate arduino pins, they are connected to the V0 pins of each other. A single potentiometer is connected in parallel to both V0 pins. This allows the to control the contrast of both LCDs at the same time.
+- The anode pin (A) on the LCD1 is connected to LCD2 instead of taking up another arduino pin. This also eliminates 4 lines of code.
+- Toggling between on/off states using a single push button, and keeping track of what the previous state was. Code can be found in the detectButton() function.
+  - This was the most challenging part of the project. The buttons and their variables are stored in arrays, and the function takes in the array index as an argument. The function returns a boolean value, and the value is stored in the previous state variable. The previous state variable is then used to determine if the button was pressed or not.
+  - This was also the most valuable takeaway from this project, as arrays are very useful for keeping track of many of the same component. 
