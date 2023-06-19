d7af871271e40c405822e2b4416cd2f07479cc77
Mumtahin Farabi <mfarabi619@gmail.com>
2023-06-19 19:11:41 -0400

    feat: Bash script creates boilerplate for .ino file depending on lcd choice

--- Scripts/create_project.sh
+++ Scripts/create_project.sh
@@ -1,50 +1,104 @@
 #!/bin/bash
+#
+# Author: Mumtahin Farabi
 
-# Get the directory of the script
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+# Main script flow
+main() {
+    check_projects_directory
+    get_directory_name
+    ask_lcd_requirement
+    create_project_directory
+    create_template_files
+    generate_ino_file_content
+    initialize_readme
+    prompt_delete_directory
+}
 
-# Determine the location of the 'Projects' directory relative to the script's location
-projects_dir="$script_dir/../Projects"
-
-# Check if the 'Projects' directory exists
-if [ ! -d "$projects_dir" ]; then
-    echo "'Projects' directory does not exist. Please create it or modify the script."
-    exit 1
-fi
-
-# Get the directory name
-read -p "Enter the directory name: " directory_name
-
-# Ask if an LCD is necessary
-lcd_1602="- "
-# Function to add LCD1602 (non-i2c) display to README
-add_lcd() {
-    read -p "Do you require an LCD1602 (non-i2c) with this project? (Y/N)" choice
-    if [[ $choice == [Yy] ]]; then
-        lcd_1602=$(
-            cat <<EOF
-- 1 x LCD1602 (non-i2c)
-- 1 x 10k Potentiometer
-- 
-EOF
-        )
+# Helper functions
+check_projects_directory() {
+    # Check if the 'Projects' directory exists
+    if [ ! -d "$projects_dir" ]; then
+        echo "'Projects' directory does not exist. Please create it or modify the script."
+        exit 1
     fi
 }
 
-add_lcd
+get_directory_name() {
+    # Get the directory name
+    read -p "Enter the directory name: " directory_name
+}
 
-# Create the directory inside the 'Projects' directory
-project_dir="$projects_dir/$directory_name"
-mkdir -p "$project_dir"
+ask_lcd_requirement() {
+    # Ask if an LCD is necessary
+    lcd_1602="- "
+    read -p "Do you require an LCD1602 (non-i2c) with this project? (Y/N)" choice
+    if [[ $choice == [Yy] ]]; then
+        lcd_1602="- 1 x LCD1602 (non-i2c)
+- 1 x 10k Potentiometer
+- "
+    fi
+}
 
-# Create template files within the directory
-# TODO: Insert boilerplate code to .ino file. Will vary depending on LCD.
-touch "$project_dir/$directory_name.ino"
-touch "$project_dir/README.md"
+create_project_directory() {
+    # Create the project directory inside the 'Projects' directory
+    project_dir="$projects_dir/$directory_name"
+    mkdir -p "$project_dir"
+}
 
-# Initialize README.md with content
-readme_content=$(
-    cat <<EOF
+create_template_files() {
+    # Create template files within the directory
+    touch "$project_dir/$directory_name.ino"
+    touch "$project_dir/README.md"
+}
+
+generate_ino_file_content() {
+    # Generate the .ino file content
+    if [[ $choice == [Yy] ]]; then
+        cat >"$project_dir/$directory_name.ino" <<EOF
+#include <LiquidCrystal.h>
+
+// LiquidCrystal(RS, E, D4, D5, D6, D7)
+// RS, E, D4, D5, D6, D7 are the LCD pins.
+LiquidCrystal LCD(2, 3, 4, 5, 6, 7);
+int LCD_BACKLIGHT_PIN = 8; // pin for LCD backlight
+
+void setup()
+{
+    analogWrite(LCD_BACKLIGHT_PIN, 100);
+    Serial.begin(9600);
+    LCD.begin(16, 2);
+}
+
+void loop()
+{
+    LCD.setCursor(0, 0);
+    LCD.print();
+    LCD.setCursor(0, 1);
+    LCD.print();
+    Serial.println();
+    delay(100);
+    LCD.clear();
+}
+EOF
+    else
+        cat >"$project_dir/$directory_name.ino" <<EOF
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+    Serial.println();
+}
+EOF
+    fi
+}
+
+initialize_readme() {
+    # Initialize README.md with content
+    readme_content=$(
+        cat <<EOF
 This project demonstrates the use of a 
 
 ## Live Demo
@@ -68,12 +122,12 @@ $lcd_1602
 - 
 
 EOF
-)
+    )
+    echo -e "$readme_content" >"$project_dir/README.md"
+}
 
-echo -e "$readme_content" >"$project_dir/README.md"
-
-# Function to delete the directory
 delete_directory() {
+    # Function to delete the directory
     read -p "Are you sure you want to delete the directory '$directory_name'? (Y/N): " choice
     if [[ $choice == [Yy] ]]; then
         rm -rf "$project_dir"
@@ -83,8 +137,19 @@ delete_directory() {
     fi
 }
 
-# Prompt to delete the directory if created by mistake
-read -p "Was the directory '$directory_name' created by mistake? Do you want to delete it? (Y/N): " delete_choice
-if [[ $delete_choice == [Yy] ]]; then
-    delete_directory
-fi
+prompt_delete_directory() {
+    # Prompt to delete the directory if created by mistake
+    read -p "Was the directory '$directory_name' created by mistake? Do you want to delete it? (Y/N): " delete_choice
+    if [[ $delete_choice == [Yy] ]]; then
+        delete_directory
+    fi
+}
+
+# Get the directory of the script
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# Determine the location of the 'Projects' directory relative to the script's location
+projects_dir="$script_dir/../Projects"
+
+# Call the main function
+main
