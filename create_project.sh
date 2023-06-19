c514568990322b26e3babf7ae36dc70e6b23f9c5
Mumtahin Farabi <mfarabi619@gmail.com>
2023-06-19 00:48:34 -0400

    feat: Bash script creates a README.md and .ino file with the option to delete the entire directory

--- create_project.sh
+++ create_project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Get the script's directory
+# Get the directory of the script
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 # Check if a directory name was provided as an argument
@@ -13,9 +13,34 @@ fi
 # Create the directory
 mkdir -p "${script_dir}/${directory_name}"
 
-# Create example files within the directory
-touch "${script_dir}/${directory_name}/file1.txt"
-touch "${script_dir}/${directory_name}/file2.txt"
+# Set permissions for the directory
+chmod 755 "${script_dir}/${directory_name}"
 
-# Grant execution permissions to the script
-chmod +x "${BASH_SOURCE[0]}"
+# Create example files within the directory
+touch "${script_dir}/${directory_name}/${directory_name}.ino"
+touch "${script_dir}/${directory_name}/README.md"
+
+# Set permissions for the example files
+chmod 644 "${script_dir}/${directory_name}/${directory_name}.ino"
+chmod 644 "${script_dir}/${directory_name}/README.md"
+
+# Initialize README.md with content
+readme_content="## ${directory_name} \n\nWelcome to the ${directory_name} directory!"
+echo -e "$readme_content" > "${script_dir}/${directory_name}/README.md"
+
+# Function to delete the directory
+delete_directory() {
+    read -p "Are you sure you want to delete the directory '${directory_name}'? (Y/N): " choice
+    if [[ $choice == [Yy] ]]; then
+        rm -rf "${script_dir}/${directory_name}"
+        echo "Directory '${directory_name}' has been deleted."
+    else
+        echo "Deletion aborted."
+    fi
+}
+
+# Prompt to delete the directory if created by mistake
+read -p "Was the directory '${directory_name}' created by mistake? Do you want to delete it? (Y/N): " delete_choice
+if [[ $delete_choice == [Yy] ]]; then
+    delete_directory
+fi
