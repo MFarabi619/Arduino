04f42c1ba70f983cbedb618ce37e23e39c75a2a2
Mumtahin Farabi <mfarabi619@gmail.com>
2023-08-09 03:16:10 -0400

    feat: Add working code

--- Projects/74HC595 Shift Register/74HC595 Shift Register.ino
+++ Projects/74HC595 Shift Register/74HC595 Shift Register.ino
@@ -1,9 +1,33 @@
+int LATCHPIN = 11;
+int CLOCKPIN = 9;
+int DATAPIN = 12;
+byte leds = 0;
 
-
-void setup() {
+void setup()
+{
+  pinMode(LATCHPIN, OUTPUT);
+  pinMode(CLOCKPIN, OUTPUT);
+  pinMode(DATAPIN, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop() {
-    Serial.println();
+void loop()
+{
+  Serial.println();
+  leds = 0;
+  updateShiftRegister();
+  delay(500);
+  for (int i = 0; i < 8; i++)
+  {
+    bitSet(leds, i);
+    updateShiftRegister();
+    delay(200);
+  }
+}
+
+void updateShiftRegister()
+{
+  digitalWrite(LATCHPIN, LOW);
+  shiftOut(DATAPIN, CLOCKPIN, LSBFIRST, leds);
+  digitalWrite(LATCHPIN, HIGH);
 }
