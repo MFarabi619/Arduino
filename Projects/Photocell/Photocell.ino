b727b75f25a4d79829c6a9dd433a499e75f99d18
Mumtahin Farabi <mfarabi619@gmail.com>
2023-08-13 15:59:56 -0400

    feat: Working Photocell code

--- Projects/Photocell/Photocell.ino
+++ Projects/Photocell/Photocell.ino
@@ -4,21 +4,27 @@
 // RS, E, D4, D5, D6, D7 are the LCD pins.
 LiquidCrystal LCD(2, 3, 4, 5, 6, 7);
 int LCD_BACKLIGHT_PIN = 8; // pin for LCD backlight
+int PHOTOCELL_PIN = A0;    // analog pin for the photocell
 
 void setup()
 {
-    analogWrite(LCD_BACKLIGHT_PIN, 100);
-    Serial.begin(9600);
-    LCD.begin(16, 2);
+    pinMode(PHOTOCELL_PIN, INPUT);     // Set the photocell pin as input
+    analogWrite(LCD_BACKLIGHT_PIN, 100); // Set LCD backlight
+    Serial.begin(9600);                 // Begin serial communication for debugging
+    LCD.begin(16, 2);                   // Start the LCD
 }
 
 void loop()
 {
+    int photocellValue = analogRead(PHOTOCELL_PIN); // Read the value from the photocell
+
     LCD.setCursor(0, 0);
-    LCD.print();
+    LCD.print("Photocell Value:"); // Display text on the first line
     LCD.setCursor(0, 1);
-    LCD.print();
-    Serial.println();
-    delay(100);
-    LCD.clear();
+    LCD.print(photocellValue);     // Display the value on the second line
+
+    Serial.println(photocellValue); // Print the value to the serial monitor
+
+    delay(1000);                   // Wait for a second
+    LCD.clear();                   // Clear the display
 }
